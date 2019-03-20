5689800ed3fe6d7170ae18981091ba75893808ee
Onez <nueng1569@hotmail.com>
2019-03-20 23:27:29 +0700

    fix item_detail screen

--- lib/tools/utils.dart
+++ lib/tools/utils.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-double baseHeight = 720.0;
+double baseHeight = 640.0;
 
 double screenAwareSize(double size, BuildContext context) {
   return size * MediaQuery.of(context).size.height/baseHeight;
--- lib/userScreens/item_detail.dart
+++ lib/userScreens/item_detail.dart
@@ -129,8 +129,8 @@ class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
             ],
           ),
           Positioned(
-            left: 18.0,
-            bottom: 15.0,
+            left: screenAwareSize(18.0, context),
+            bottom: screenAwareSize(15.0, context),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
@@ -138,7 +138,7 @@ class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
                   'Rating',
                   style: TextStyle(
                     color: Color(0xFF949598),
-                    fontSize: 10.0,
+                    fontSize: screenAwareSize(10.0, context),
                   ),
                 ),
                 SizedBox(
@@ -161,7 +161,7 @@ class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
                       "${widget.itemRating}",
                       style: TextStyle(
                         color: Color(0xFFFFE600),
-                        fontSize: 10.0,
+                        fontSize: screenAwareSize(10.0, context),
                       ),
                     ),
                     SizedBox(
@@ -171,7 +171,7 @@ class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
                       '(370 People)',
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 10.0,
+                        fontSize: screenAwareSize(10.0, context),
                       ),
                     )
                   ],
@@ -208,6 +208,20 @@ class ProductScreenBottomPart extends StatefulWidget {
 
 class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
   bool isExpanded = false;
+  int currentSizeIndex = 0;
+  int _counter = 1;
+
+  void _increase() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrease() {
+    setState(() {
+      if (_counter > 1) _counter--;
+    });
+  }
 
   void _expand() {
     setState(() {
@@ -223,12 +237,14 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 18.0),
+            padding: EdgeInsets.only(
+                top: screenAwareSize(10.0, context),
+                left: screenAwareSize(18.0, context)),
             child: Text(
               'Product Description',
               style: TextStyle(
                 color: Color(0xFF949598),
-                fontSize: 10.0,
+                fontSize: screenAwareSize(10.0, context),
               ),
             ),
           ),
@@ -236,21 +252,23 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
             height: 8.0,
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 26.0, right: 18.0),
+            padding: EdgeInsets.only(
+                left: screenAwareSize(26.0, context),
+                right: screenAwareSize(18.0, context)),
             child: AnimatedCrossFade(
               firstChild: Text(
                 widget.itemDesc,
                 maxLines: 2,
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 10.0,
+                  fontSize: screenAwareSize(10.0, context),
                 ),
               ),
               secondChild: Text(
                 widget.itemDesc,
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 10.0,
+                  fontSize: screenAwareSize(10.0, context),
                 ),
               ),
               crossFadeState: isExpanded
@@ -260,7 +278,9 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 26.0, right: 18.0),
+            padding: EdgeInsets.only(
+                left: screenAwareSize(26.0, context),
+                right: screenAwareSize(18.0, context)),
             child: GestureDetector(
               onTap: _expand,
               child: Text(
@@ -268,7 +288,7 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
                 style: TextStyle(
                   color: Color(0xFFFB382F),
                   fontWeight: FontWeight.w700,
-                  fontSize: 10.0,
+                  fontSize: screenAwareSize(10.0, context),
                 ),
               ),
             ),
@@ -277,9 +297,9 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
             height: 12.0,
           ),
           Padding(
-            padding: const EdgeInsets.only(
-              left: 18.0,
-              right: 75.0,
+            padding: EdgeInsets.only(
+              left: screenAwareSize(18.0, context),
+              right: screenAwareSize(90.0, context),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -288,32 +308,200 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
                   'Size',
                   style: TextStyle(
                     color: Color(0xFF949598),
-                    fontSize: 10.0,
+                    fontSize: screenAwareSize(10.0, context),
                   ),
                 ),
                 Text(
                   'Quantity',
                   style: TextStyle(
                     color: Color(0xFF949598),
-                    fontSize: 10.0,
+                    fontSize: screenAwareSize(10.0, context),
                   ),
                 ),
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: widget.sizeList.map((item) {
-                  return GestureDetector(
-                    onTap: () {},
-                    child: sizeItem(),
-                  );
-                }).toList(),
+          Padding(
+            padding: EdgeInsets.only(
+                left: screenAwareSize(16.0, context),
+                right: screenAwareSize(13.0, context)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: screenAwareSize(38.0, context),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: widget.sizeList.map((item) {
+                      var index = widget.sizeList.indexOf(item);
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            currentSizeIndex = index;
+                          });
+                        },
+                        child:
+                            sizeItem(item, index == currentSizeIndex, context),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Container(
+                    width: screenAwareSize(100.0, context),
+                    height: screenAwareSize(30.0, context),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF525663),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Flexible(
+                          flex: 3,
+                          child: GestureDetector(
+                            onTap: _decrease,
+                            child: Container(
+                              height: double.infinity,
+                              child: Center(
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        divider(),
+                        Flexible(
+                          flex: 3,
+                          child: Container(
+                            height: double.infinity,
+                            child: Center(
+                              child: Text(
+                                _counter.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        divider(),
+                        Flexible(
+                          flex: 3,
+                          child: GestureDetector(
+                            onTap: _increase,
+                            child: Container(
+                              height: double.infinity,
+                              child: Center(
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: screenAwareSize(18.0, context)),
+            child: Text(
+              'Price',
+              style: TextStyle(
+                fontSize: screenAwareSize(10.0, context),
+                color: Color(0xFF949598),
               ),
-            ],
+            ),
+          ),
+          SizedBox(
+            height: screenAwareSize(8.0, context),
+          ),
+          Container(
+            width: double.infinity,
+            height: screenAwareSize(120.0, context),
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: screenAwareSize(22.0, context)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: screenAwareSize(18.0, context)),
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              '250',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenAwareSize(26.0, context),
+                              ),
+                            ),
+                            SizedBox(
+                              width: screenAwareSize(2.0, context),
+                            ),
+                            Text(
+                              ' Baht',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenAwareSize(26.0, context),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: screenAwareSize(10.0, context),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: MaterialButton(
+                          color: Color(0xFFFB382F),
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenAwareSize(14.0, context),
+                          ),
+                          onPressed: () {},
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: screenAwareSize(35.0, context)),
+                              child: Text(
+                                'Add To Cart',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: screenAwareSize(15.0, context),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -322,16 +510,41 @@ class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
 }
 
 Widget sizeItem(String size, bool isSelected, BuildContext context) {
-  return Container(
-    width: 30.0,
-    height: 30.0,
-    decoration: BoxDecoration(
-      color: isSelected ? Color(0xFFFC3930) : Color(0xFF525663),
-      boxShadow: [
-        BoxShadow(
-          color: isSelected ? Colors.black.withOpacity(.5) : Colors.black12,
+  return Padding(
+    padding: EdgeInsets.only(left: screenAwareSize(12.0, context)),
+    child: Container(
+      width: screenAwareSize(30.0, context),
+      height: screenAwareSize(30.0, context),
+      decoration: BoxDecoration(
+        color: isSelected ? Color(0xFFFC3930) : Color(0xFF525663),
+        borderRadius: BorderRadius.circular(5.0),
+        boxShadow: [
+          BoxShadow(
+            color: isSelected ? Colors.black.withOpacity(.5) : Colors.black12,
+            offset: Offset(0.0, 10.0),
+            blurRadius: 10.0,
+          ),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          size,
+          style: TextStyle(
+            fontSize: screenAwareSize(10.0, context),
+            color: Colors.white,
+          ),
         ),
-      ],
+      ),
+    ),
+  );
+}
+
+Widget divider() {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+    child: Container(
+      width: 0.8,
+      color: Colors.black,
     ),
   );
 }
